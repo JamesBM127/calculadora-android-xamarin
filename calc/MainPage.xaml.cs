2af2657173cc602013324abce7fc061cbc92fc36
Jameson Brandao Martins <jamesonmartins127@gmail.com>
2020-11-07 23:45:48 -0400

    Clicked event in numbers button

--- calc/MainPage.xaml.cs
+++ calc/MainPage.xaml.cs
@@ -1,4 +1,4 @@
-﻿using System;
+﻿ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -12,9 +12,53 @@ namespace calc
     [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class MainPage : ContentPage
     {
+        int currentState = 1;
+        string mathOperator;
+        double firstNumber, secondNumber;
+
+        private void OnClear(object sender, EventArgs e)
+        {
+            firstNumber = 0;
+            secondNumber = 0;
+            currentState = 1;
+            this.resultText.Text = "0";
+        }
+
+        private void OnSelectNumber(object sender, EventArgs e)
+        {
+            Button button = (Button)sender;
+            string pressed = button.Text;
+
+            if(this.resultText.Text == "0" || currentState < 0)
+            {
+                this.resultText.Text = "";
+                if (currentState < 0) currentState *= -1;
+            }
+
+            this.resultText.Text += pressed;
+
+            double number;
+            if(double.TryParse(this.resultText.Text, out number))
+            {
+                this.resultText.Text = number.ToString("N0");
+                if (currentState == 1) firstNumber = number;
+                else secondNumber = number;
+            }
+            
+        }
+
+        private void OnSelectOperator(object sender, EventArgs e)
+        {
+            currentState = -2;
+            Button button = (Button)sender;
+            string pressed = button.Text;
+            mathOperator = pressed;
+        }
+
         public MainPage()
         {
             InitializeComponent();
+            OnClear(new object(), new EventArgs());
         }
     }
 }
