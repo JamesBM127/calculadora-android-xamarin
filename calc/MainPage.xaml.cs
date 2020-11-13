a2d1bc9615bd40f97978c6c76a12a1e0cf30af96
Jameson Brandao Martins <jamesonmartins127@gmail.com>
2020-11-13 16:43:15 -0400

    Percent button instantiated

--- calc/MainPage.xaml.cs
+++ calc/MainPage.xaml.cs
@@ -12,18 +12,37 @@ namespace calc
     [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class MainPage : ContentPage
     {
-        int currentState = 1;
-        string mathOperator;
+        sbyte currentState = 1; //-128..127
+        string mathOperator = null;
         double firstNumber, secondNumber;
+        bool firstOperator = true;
+        bool percentPressed = false;
 
         private void OnClear(object sender, EventArgs e)
         {
+            firstOperator = true;
             firstNumber = 0;
             secondNumber = 0;
             currentState = 1;
             this.resultText.Text = "0";
+            this.resultHistory.Text = "";
             mathOperator = null;
+            percentPressed = false;
         }
+
+        /* ******************************************************************************************************************************
+        *****************************************************  OnCancelEntry  *************************************************** 
+        ****************************************************************************************************************************** */
+        private void OnCancelEntry(object sender, EventArgs e)
+        {
+            this.resultText.Text = "0";
+            secondNumber = 0;
+            currentState = -2;
+        }
+
+        /* ******************************************************************************************************************************
+        *****************************************************   OnSelectNumber   *************************************************** 
+        ****************************************************************************************************************************** */
 
         private void OnSelectNumber(object sender, EventArgs e)
         {
@@ -44,11 +63,22 @@ namespace calc
                 if (currentState == 1) firstNumber = number;
                 else secondNumber = number;
             }
-            
+
         }
+
+        /* ******************************************************************************************************************************
+            *****************************************************   OnSelectOperator   *************************************************** 
+            ****************************************************************************************************************************** */
 
         private void OnSelectOperator(object sender, EventArgs e)
         {
+            if (!firstOperator)
+            {
+                OnCalculate(new object(), new EventArgs());
+                this.resultHistory.Text = this.resultHistory.Text.Remove(this.resultHistory.Text.Length - 1);
+                this.resultHistory.Text += mathOperator;
+            }
+
             currentState = -2;
             Button button = (Button)sender;
             if (mathOperator == button.Text) return;
@@ -60,10 +90,30 @@ namespace calc
             }
             mathOperator = button.Text;
             this.resultText.Text += mathOperator;
+
+            if (this.resultHistory.Text.EndsWith("="))
+            {
+                string rHistory = this.resultHistory.Text.Remove(this.resultHistory.Text.Length - 1);
+                this.resultHistory.Text = "";
+                //this.resultHistory.Text = rHistory;
+            }
+            this.resultHistory.Text += this.resultText.Text;
+
+            firstOperator = false;
         }
+
+        /* ******************************************************************************************************************************
+        *****************************************************   OnCalculate   *************************************************** 
+        ****************************************************************************************************************************** */
 
         private void OnCalculate(object sender, EventArgs e)
         {
+            if (percentPressed)
+            {
+                OnPercent(new object(), new EventArgs());
+                return;
+            }
+
             if(currentState == 2)
             {
                 double result = 0;
@@ -75,9 +125,16 @@ namespace calc
                     case "/": result = firstNumber / secondNumber; break;
                 }
 
-                string resultString = result.ToString("N10");
+                this.resultHistory.Text += secondNumber.ToString() + "=";
 
-                while (resultString.EndsWith("0")){
+                string resultString = result.ToString("N10");
+                
+                while (resultString.EndsWith("0") || resultString.EndsWith(",")){
+                    resultString = resultString.Remove(resultString.Length - 1);
+                }
+
+                if (resultString.EndsWith("."))
+                {
                     resultString = resultString.Remove(resultString.Length - 1);
                 }
 
@@ -86,6 +143,46 @@ namespace calc
                 firstNumber = result;
                 currentState = -1;
             }
+            else
+            {
+                this.resultHistory.Text = firstNumber.ToString() + "=";
+            }
+        }
+
+        /* ******************************************************************************************************************************
+        ********************************************************   OnPercent   ********************************************************
+        ****************************************************************************************************************************** */
+
+        private void OnPercent(object sender, EventArgs e)
+        {
+            if (!percentPressed)
+            {
+                if (mathOperator == "x" || mathOperator == "/")
+                {
+                    this.resultHistory.Text = firstNumber.ToString() + mathOperator + (secondNumber / 100).ToString();
+                    this.resultText.Text = (secondNumber / 100).ToString();
+                }
+                else
+                {
+                    this.resultHistory.Text = firstNumber.ToString() + mathOperator + (firstNumber * secondNumber / 100).ToString();
+                    this.resultText.Text = (firstNumber * secondNumber / 100).ToString();
+                }
+                percentPressed = true;
+                return;
+            }
+
+            double result = 0;
+            switch (mathOperator)
+            {
+                case "+": result = firstNumber + (firstNumber * secondNumber / 100); break;
+                case "-": result = firstNumber - (firstNumber * secondNumber / 100); break;
+                case "x": result = firstNumber * secondNumber / 100; break;
+                case "/": result = firstNumber / secondNumber / 100; break;
+            }
+
+            this.resultHistory.Text += "=";
+            this.resultText.Text = result.ToString();
+
         }
 
         public MainPage()
